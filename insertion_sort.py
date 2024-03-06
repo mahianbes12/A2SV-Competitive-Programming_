00a4db0a1ec4149658b5490698d16497d00e9e7e
Mahlet Anbessie <mahletanbessie@gmail.com>
2024-03-06 21:46:36 +0300

    insertio_sort-HackerRank-problem

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,15 +1,15 @@
 #Insertion Sort - Part 1 - HackerRank
 def insertionSort1(n, arr):
-    value = arr[n-1]  # Store the value to be inserted
-    i = n - 2  # Start comparing from the second-to-last element
+    value = arr[n-1]
+    i = n - 2
 
     while i >= 0 and arr[i] > value:
-        arr[i+1] = arr[i]  # Shift elements to the right
+        arr[i+1] = arr[i]
         i -= 1
-        print(*arr)  # Print the array after each shift
+        print(*arr)
 
-    arr[i+1] = value  # Insert the stored value at the correct position
-    print(*arr)  # Print the final sorted array
+    arr[i+1] = value
+    print(*arr)
 
 
 # Example usage
